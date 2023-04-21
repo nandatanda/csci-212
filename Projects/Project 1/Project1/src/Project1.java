f1b261d1aa0b78990d7aebb241dd748e90b1edb5
Ali Elayni <aelayni@outlook.com>
2023-04-21 17:53:30 -0400

    Flatten Project 1 directory

--- Projects/Project 1/Project1/src/Project1.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Project1 {
-    public static void main(String[] args) {
-        RomanNumeralGUI window = new RomanNumeralGUI();
-        window.setLocation(200, 200);
-        window.setDefaultCloseOperation(RomanNumeralGUI.EXIT_ON_CLOSE);
-    }
-}
