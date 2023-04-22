2967b03b7374b922091f01b6fc3ac684321a93cb
Ali Elayni <aelayni@outlook.com>
2023-04-22 12:12:33 -0400

    Respect class rename

--- Projects/Project 3/src/FileMenuHandler.java
+++ Projects/Project 3/src/FileMenuHandler.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 
 public class FileMenuHandler implements ActionListener {
     JFrame jframe;
@@ -15,11 +14,11 @@ public class FileMenuHandler implements ActionListener {
         String filename = "";
         if (menuName.equals("Open")) {
             filename = JOptionPane.showInputDialog("Enter the location of the file to be read:");
-            FileReader file = new FileReader(filename);
+            RomanFile file = new RomanFile(filename);
 
             Project3.window.clearAll();
             Project3.window.addRomanNumerals(file.toUnsortedRoman());
-            Project3.window.addUnsortedArabicNumerals(file.toUnsortedArabic());
+            Project3.window.setArabicText(file.toUnsortedArabic());
             Project3.window.addSortedArabicNumerals(file.toSortedArabic());
         } else if (menuName.equals("Quit")) {
             JOptionPane.showMessageDialog(null, "Thank you for using my converter!");
