4389f0a0df74a5ea7810afc524a6e01ef01d491c
Ali Elayni <aelayni@outlook.com>
2023-04-22 12:44:01 -0400

    Replace FileMenuHandler with sample from course website

--- Projects/Project 3/src/FileMenuHandler.java
+++ Projects/Project 3/src/FileMenuHandler.java
@@ -1,27 +1,50 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 public class FileMenuHandler implements ActionListener {
     JFrame jframe;
-
-    public FileMenuHandler(JFrame jf) {
+    public FileMenuHandler (JFrame jf) {
         jframe = jf;
     }
-
     public void actionPerformed(ActionEvent event) {
-        String menuName = event.getActionCommand();
-        String filename = "";
-        if (menuName.equals("Open")) {
-            filename = JOptionPane.showInputDialog("Enter the location of the file to be read:");
-            RomanFile file = new RomanFile(filename);
-
-            Project3.window.setRomanText(file.toUnsortedRoman());
-            Project3.window.setArabicText(file.toUnsortedArabic());
-            Project3.window.setSortedArabicText(file.toSortedArabic());
-        } else if (menuName.equals("Quit")) {
-            JOptionPane.showMessageDialog(null, "Thank you for using my converter!");
+        String  menuName;
+        menuName = event.getActionCommand();
+        if (menuName.equals("Open"))
+            openFile( );
+        else if (menuName.equals("Quit"))
             System.exit(0);
-        }
+    } //actionPerformed
+
+    private void openFile( ) {
+        JFileChooser chooser;
+        int          status;
+        chooser = new JFileChooser( );
+        status = chooser.showOpenDialog(null);
+        if (status == JFileChooser.APPROVE_OPTION)
+            readSource(chooser.getSelectedFile());
+        else
+            JOptionPane.showMessageDialog(null, "Open File dialog canceled");
+    } //openFile
+
+    private void readSource(File chosenFile) {
+        String chosenFileName = chosenFile.getName();
+        TextFileInput inFile = new TextFileInput(chosenFileName);
+        String ssn;
+        int subscript = 0;
+        Container myContentPane = jframe.getContentPane();
+        TextArea myTextArea = new TextArea();
+        TextArea mySubscripts = new TextArea();
+        myContentPane.add(myTextArea, BorderLayout.EAST);
+        myContentPane.add(mySubscripts, BorderLayout.WEST);
+
+        ssn = inFile.readLine();
+        while (ssn != null) {
+            mySubscripts.append(Integer.toString(subscript++)+"\n");
+            myTextArea.append(ssn+"\n");
+            ssn = inFile.readLine();
+        } //while
+        //    jframe.setVisible(true);
+        jframe.setVisible(true);
     }
 }
