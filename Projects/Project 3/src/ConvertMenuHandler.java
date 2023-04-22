4ac620ba49082bdcf884d1a11f9aee25c8f342bd
Ali Elayni <aelayni@outlook.com>
2023-04-22 15:47:15 -0400

    Documentation

--- Projects/Project 3/src/ConvertMenuHandler.java
+++ Projects/Project 3/src/ConvertMenuHandler.java
@@ -10,14 +10,17 @@ public class ConvertMenuHandler implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent event) {
+        // Store the latest action
         String menuName = event.getActionCommand();
 
-        // Prompt for a roman numeral, convert to arabic, and display the result
+        // Check if that action was to click the "Roman to arabic" button
         if (menuName.equals("Roman to arabic")) {
             try {
+                // Create a Roman numeral object from user input, which we use to convert to an Arabic string
                 RomanNumeral r = new RomanNumeral(JOptionPane.showInputDialog("Enter a Roman numeral:"));
                 JOptionPane.showMessageDialog(null, "The Roman numeral '" + r.getNumeral() + "' carries the value " + r.toArabicString());
             } catch (IllegalRomanNumeralException e) {
+                // Inform user of invalid input and print the offending string to the console
                 JOptionPane.showMessageDialog(null, "The Roman numeral you entered is invalid");
                 System.out.println(e.getMessage());
             }
