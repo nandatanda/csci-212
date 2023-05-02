4fbc0e5556e5a6189935a2b36f41f8b6bbc6082a
Ali Elayni <aelayni@outlook.com>
2023-05-02 17:02:09 -0400

    Update Question4.java

--- Exams/Exam 2/src/Question4.java
+++ Exams/Exam 2/src/Question4.java
@@ -1,8 +1,4 @@
-public class Question4 {
-
-}
-
-public interface Flapping {
+interface Flapping {
     public static final int DEFAULT_WING_BEATS = 30;
     public static final int DEFAULT_POPULATION = 10000;
 
@@ -11,7 +7,7 @@ public interface Flapping {
     public boolean flaps();
 }
 
-public abstract class Animal {
+abstract class Animal {
     String species;
     int population;
 
@@ -23,16 +19,26 @@ public abstract class Animal {
         species = s;
         population = p;
 
-        System.out.println("New animal, species is " + species + ", population" + population);
+        System.out.println("New animal, species is " + species + ", population " + population);
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setPopulation(int p) {
+        if (p < 0)
+            throw new InvalidAnimalException("Population cannot be negative");
+        population = p;
     }
 }
 
-public abstract class FlyingAnimal extends Animal implements Flapping {
+abstract class FlyingAnimal extends Animal implements Flapping {
     protected int wingBeatsPerSecond;
 
     public FlyingAnimal(String name, int population, int w) {
         super(name, population);
-        if (w < 1) throw new FlyingAnimalException("Wing beats < 1");
+        if (w < 1) throw new InvalidAnimalException("Wing beats must be greater than zero");
         else
             wingBeatsPerSecond = w;
     }
@@ -42,11 +48,17 @@ public abstract class FlyingAnimal extends Animal implements Flapping {
     }
 }
 
-public class Bird extends FlyingAnimal {
+class InvalidAnimalException extends IllegalArgumentException {
+    public InvalidAnimalException(String message) {
+        super(message);
+    }
+}
+
+class Bird extends FlyingAnimal {
     private boolean flapping = true;
 
     public Bird(String species) {
-        super(species, Flapping.DEFAULT_WING_BEATS, Flapping.DEFAULT_POPULATION);
+        super(species, Flapping.DEFAULT_POPULATION, Flapping.DEFAULT_WING_BEATS);
     }
 
     public Bird(String species, int population, int wingBeats) {
@@ -65,7 +77,7 @@ public class Bird extends FlyingAnimal {
     }
 }
 
-public class Bat extends FlyingAnimal {
+class Bat extends FlyingAnimal {
     private boolean flapping = false;
 
     public Bat(String species) {
@@ -85,3 +97,14 @@ public class Bat extends FlyingAnimal {
 
     public boolean flaps() {
         return flapping;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Bat igor = null;
+        Bird robin;
+        FlyingAnimal finch;
+        igor = new Bat("Vampire", 1200, 30);
+    }
+}
