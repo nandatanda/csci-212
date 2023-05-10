17fc5024eb4f2a953e9325123cbca07e867ce61f
Ali Elayni <aelayni@outlook.com>
2023-05-09 20:48:20 -0400

    Test RomanNumeral class

--- Projects/Project 4/src/Project4.java
+++ Projects/Project 4/src/Project4.java
@@ -2,6 +2,7 @@ public class Project4 {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello world!");
+        RomanNumeral myNum = new RomanNumeral("QED");
+        System.out.println(myNum.getSymbol() + "\n" + myNum.getValueString());
     }
 }
